5578005dc20305a0c7fcce0533c3a1c05de5727a
KyungJun.Woo <natcho9010@gmail.com>
2024-12-11 23:55:03 +0900

    asdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
-import GUI from 'lil-gui';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 function Page() {
